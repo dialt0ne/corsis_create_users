3fa78f96057e9f7904c84fc69651832ea8da45bf
dialt0ne <anthony@tonns.com>
2014-02-03 17:03:15 -0500

    correct version bump

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'atonns@corsis.com'
 license          'Apache 2.0'
 description      'Installs/Configures corsis_create_users'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.1'
 recipe            "corsis_create_users", "Creates users, sets authorized_keys for them from S3"
 depends          "s3"
